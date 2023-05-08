c8bcd0e31244c8722cd4e4af44853d8b09ad8f39
Tim Herzig <tim.herzig@hotmail.com>
2023-05-08 18:31:46 +0200

    starting skeleton of model

---
+++ src/model.py
@@ -0,0 +1,36 @@
+import torch
+
+from torch import nn
+from lightning.pytorch import LightningModule
+from swin.swin_transformer import SwinTransformer3D
+
+class Model(LightningModule):
+    '''
+    '''
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.swin_encoder = SwinTransformer3D(
+            pretrained=config.model.swin.pretrained,
+            use_checkpoint=config.model.swin.use_checkpoint
+        )
+
+    def forward(self, x):
+        return
+
+    def configure_optimizers(self):
+        opt = torch.optim.AdamW(self.model.parameters(), lr=1e-3)
+        return opt
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+
+        
+
+        self.log('train_loss', loss)
+        return loss
+    
+    # def backward(self, trainer, loss, optimizer, optimizer_idx):
+    #     loss.backward()
