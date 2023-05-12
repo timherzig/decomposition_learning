16f006985c51c948ee876ef66e22f1c99f2ab3ae
sudo-Boris <boris.meinardus00@gmail.com>
2023-05-12 11:33:31 +0200

    Implement Wandb logging - V1 Basic loss logging. Fix bug in Dataset obj to not read int for filename.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import lightning.pytorch as pl
+from lightning.pytorch.loggers import WandbLogger
 from omegaconf import OmegaConf
 
 # from src.model import Model
@@ -6,19 +7,26 @@ from utils.parser import parse_arguments
 from models.decomposer import Decomposer
 from data.siar_data import SIARDataModule
 
+
 def main(args):
     config = OmegaConf.load(args.config)
+    wandb_logger = WandbLogger(config=config, project="HTCV")
 
     siar = SIARDataModule(config.data.dir, 2)
-    siar.setup('train')
+    siar.setup("train")
 
     model = Decomposer(config=config.model)
-    trainer = pl.Trainer(max_epochs=1)
-    trainer.fit(model, train_dataloaders=siar.train_dataloader(), val_dataloaders=siar.val_dataloader())
+    trainer = pl.Trainer(max_epochs=1, logger=wandb_logger)
+    trainer.fit(
+        model,
+        train_dataloaders=siar.train_dataloader(),
+        val_dataloaders=siar.val_dataloader(),
+    )
 
     return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_arguments()
-    
+
     main(args)
