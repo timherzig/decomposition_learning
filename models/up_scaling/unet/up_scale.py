e002bf775ab0d213c1bc9b653d7fccc714d55131
Tim Herzig <tim.herzig@hotmail.com>
2023-05-19 13:21:34 +0200

    move dummy features to device

--- models/up_scaling/unet/up_scale.py
+++ models/up_scaling/unet/up_scale.py
@@ -80,7 +80,7 @@ class UpSampler(nn.Module):
             dummy_features = torch.zeros(
                 (1, 1, output_size[2], output_size[3], output_size[4])
             )
-            dummy_features.to(x.device)
+            dummy_features = dummy_features.to(x.device)
             print(f"x device {x.device}, dummy device {dummy_features.device}")
             x = layer(dummy_features, x, skip_joining=True)
         # Dim. after upscaling: Batch x _ x 10 x 256 x 256
