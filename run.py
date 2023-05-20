5153818547a82adb81357593732429ac6aacb2ae
Marii19 <m.trzeciakiewicz14@gmail.com>
2023-05-20 12:33:19 +0200

    ready for tests on cluster

--- run.py
+++ run.py
@@ -10,27 +10,35 @@ from data.siar_data import SIARDataModule
 
 def main(args):
     config = OmegaConf.load(args.config)
-    wandb_logger = WandbLogger(config=config, project="HTCV")
 
     siar = SIARDataModule(config.data.dir, config.train.batch_size)
     siar.setup("train", config.train.debug)
 
     model = Decomposer(config=config.model)
-    trainer = pl.Trainer(
-        max_epochs=config.train.max_epochs,
-        logger=wandb_logger,
-        default_root_dir="checkpoints",
-        accelerator="gpu",
-    )
 
+    if config.wandb.logger:
+        wandb_logger = WandbLogger(config=config, project="HTCV")
+
+        trainer = pl.Trainer(
+            max_epochs=config.train.max_epochs,
+            logger=wandb_logger,
+            default_root_dir="checkpoints",
+            log_every_n_steps=config.train.log_every_n_steps
+        )
+    else:
+        trainer = pl.Trainer(
+            max_epochs=config.train.max_epochs,
+            default_root_dir="checkpoints",
+            log_every_n_steps=config.train.log_every_n_steps)
+        
     trainer.fit(
         model,
         train_dataloaders=siar.train_dataloader(),
         val_dataloaders=siar.val_dataloader(),
     )
 
-    siar.setup("test", config.train.debug)
-    trainer.test(model, test_dataloaders=siar.test_dataloader())
+    # siar.setup("test", config.train.debug)
+    # trainer.test(model, test_dataloaders=siar.test_dataloader())
 
     return
 
