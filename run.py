15fdbd9cc94101252ecb1d86f0ab163f8571282c
Tim Herzig <tim.herzig@hotmail.com>
2023-05-24 11:27:49 +0200

    wandblogger added back

--- run.py
+++ run.py
@@ -22,6 +22,7 @@ def get_git_commit():
 
 def main(args):
     config = OmegaConf.load(args.config)
+    wandb_logger = WandbLogger(config=config, project="HTCV")
 
     siar = SIARDataModule(config.data.dir, config.train.batch_size)
     siar.setup("train", config.train.debug)
