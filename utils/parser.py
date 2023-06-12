eeb2934a4f8365dfe843cca12c03ee47babdab9b
Tim Herzig <tim.herzig@hotmail.com>
2023-06-12 10:52:45 +0200

    parser

--- utils/parser.py
+++ utils/parser.py
@@ -4,6 +4,6 @@ from argparse import ArgumentParser
 def parse_arguments():
     parser = ArgumentParser()
 
-    parser.add_argument("--config", type=str, help="", default="configs/default.yaml")
+    parser.add_argument("--config", type=str, help="", default="config/default.yaml")
 
     return parser.parse_args("")
