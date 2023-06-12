4ec181538ff9185cde4f3c8b94936f520fe34d77
sudo-Boris <boris.meinardus00@gmail.com>
2023-06-12 17:34:56 +0200

    Set default config to swin_swin. Easier for debugging swin upsampler.

--- utils/parser.py
+++ utils/parser.py
@@ -1,8 +1,9 @@
 from argparse import ArgumentParser
 
+
 def parse_arguments():
     parser = ArgumentParser()
 
-    parser.add_argument('--config', type=str, help='', default='config/default.yaml')
+    parser.add_argument("--config", type=str, help="", default="config/swin_swin.yaml")
 
     return parser.parse_args()
