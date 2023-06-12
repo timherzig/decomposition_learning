c2d23c09107ccfdd698bab3fb1ca7baca5f9a4e0
sudo-Boris <boris.meinardus00@gmail.com>
2023-06-12 17:34:28 +0200

    Fix device bug for upsampling unit. Previously, final projection layer was initialized every model forward.

--- models/up_scaling/reverse_st/upsampling.py
+++ models/up_scaling/reverse_st/upsampling.py
@@ -5,50 +5,55 @@ from einops import rearrange
 
 
 class PatchSplitting(nn.Module):
-
-    def __init__(self, dim, norm_layer=nn.LayerNorm): 
+    def __init__(self, dim, norm_layer=nn.LayerNorm):
         super().__init__()
         self.dim = dim
-        self.expansion = nn.Linear(dim, 2*dim, bias=False)
-        self.norm = norm_layer(dim//2)
+        self.expansion = nn.Linear(dim, 2 * dim, bias=False)
+        self.norm = norm_layer(dim // 2)
 
-    def forward(self, x): 
+    def forward(self, x):
         device = x.device
         x = self.expansion(x.to(device))
-        x0, x1, x2, x3 = torch.split(x, x.shape[-1]//4, dim = -1)
-        merged = torch.empty((x0.shape[0], x0.shape[1], x0.shape[2]*2, x0.shape[3]*2, x0.shape[4])).to(device)
+        x0, x1, x2, x3 = torch.split(x, x.shape[-1] // 4, dim=-1)
+        merged = torch.empty(
+            (x0.shape[0], x0.shape[1], x0.shape[2] * 2, x0.shape[3] * 2, x0.shape[4])
+        ).to(device)
         merged[:, :, 0::2, 0::2, :] = x0
         merged[:, :, 0::2, 1::2, :] = x1
         merged[:, :, 1::2, 0::2, :] = x2
         merged[:, :, 1::2, 1::2, :] = x3
 
         merged = self.norm(merged)
-        #print(merged.shape)
+        # print(merged.shape)
 
         return merged
 
+
 class SwinTransformer3D_up(SwinTransformer3D):
-    def __init__(self,
-                 pretrained=None,
-                 pretrained2d=True,
-                 patch_size=(4,4,4),
-                 in_chans=3,      
-                 embed_dim=768,   # changed
-                 depths=[2, 6, 2, 2], # change?
-                 num_heads=[24, 12, 6, 3],  # reversed order
-                 window_size=(2,7,7),
-                 mlp_ratio=4.,
-                 qkv_bias=True,
-                 qk_scale=None,
-                 drop_rate=0.,
-                 attn_drop_rate=0.,
-                 drop_path_rate=0.2,
-                 norm_layer=nn.LayerNorm,
-                 patch_norm=False,
-                 frozen_stages=-1,
-                 use_checkpoint=False):
+    def __init__(
+        self,
+        pretrained=None,
+        pretrained2d=True,
+        patch_size=(4, 4, 4),
+        in_chans=3,
+        out_chans=3,
+        embed_dim=768,  # changed
+        depths=[2, 6, 2, 2],  # change?
+        num_heads=[24, 12, 6, 3],  # reversed order
+        window_size=(2, 7, 7),
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.2,
+        norm_layer=nn.LayerNorm,
+        patch_norm=False,
+        frozen_stages=-1,
+        use_checkpoint=False,
+    ):
         super().__init__()
-        
+
         self.pretrained = pretrained
         self.pretrained2d = pretrained2d
         self.num_layers = len(depths)
@@ -57,6 +62,7 @@ class SwinTransformer3D_up(SwinTransformer3D):
         self.frozen_stages = frozen_stages
         self.window_size = window_size
         self.patch_size = patch_size
+        self.out_chans = out_chans
 
         # # split image into non-overlapping patches
         # self.patch_embed = PatchEmbed3D(
@@ -66,13 +72,15 @@ class SwinTransformer3D_up(SwinTransformer3D):
         self.pos_drop = nn.Dropout(p=drop_rate)
 
         # stochastic depth
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
+        dpr = [
+            x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))
+        ]  # stochastic depth decay rule
 
         # build layers
         self.layers = nn.ModuleList()
         for i_layer in range(self.num_layers):
             layer = BasicLayer(
-                dim=int(embed_dim / 2**i_layer),   #changed from *2 to /2
+                dim=int(embed_dim / 2**i_layer),  # changed from *2 to /2
                 depth=depths[i_layer],
                 num_heads=num_heads[i_layer],
                 window_size=window_size,
@@ -81,13 +89,36 @@ class SwinTransformer3D_up(SwinTransformer3D):
                 qk_scale=qk_scale,
                 drop=drop_rate,
                 attn_drop=attn_drop_rate,
-                drop_path=dpr[sum(depths[:i_layer]):sum(depths[:i_layer + 1])],
+                drop_path=dpr[sum(depths[:i_layer]) : sum(depths[: i_layer + 1])],
                 norm_layer=norm_layer,
-                downsample=PatchSplitting if i_layer<self.num_layers-1 else None,  #changed
-                use_checkpoint=use_checkpoint)
+                downsample=PatchSplitting
+                if i_layer < self.num_layers - 1
+                else None,  # changed
+                use_checkpoint=use_checkpoint,
+            )
             self.layers.append(layer)
 
-        self.num_features = int(embed_dim / 2**(self.num_layers-1))   #changed from *2 to /2
+        self.num_features = int(
+            embed_dim / 2 ** (self.num_layers - 1)
+        )  # changed from *2 to /2
+
+        if self.out_chans == 3:
+            self.proj = torch.nn.ConvTranspose3d(
+                self.num_features,
+                self.out_chans,
+                (5, 4, 4),
+                (1, 4, 4),
+                padding=(4, 0, 0),
+                dilation=(1, 1, 1),
+            )
+        else:
+            self.proj = torch.nn.ConvTranspose3d(
+                self.num_features,
+                self.out_chans,
+                (2, 4, 4),
+                (2, 4, 4),
+                dilation=(1, 1, 1),
+            )
 
         # add a norm layer for each output
         self.norm = norm_layer(self.num_features)
@@ -108,33 +139,25 @@ class SwinTransformer3D_up(SwinTransformer3D):
                 for param in m.parameters():
                     param.requires_grad = False
 
-
-    def forward(self, x, channels):
+    def forward(self, x):
         """Forward function."""
-        #x = self.patch_embed(x)
-        #print("after patch parition: ", x.shape)
-        #x = self.pos_drop(x)     # needed? 
+        # x = self.patch_embed(x)
+        # print("after patch parition: ", x.shape)
+        # x = self.pos_drop(x)     # needed?
 
         for idx, layer in enumerate(self.layers):
             x, _ = layer(x.contiguous())
             # print("Layer nr:" ,str(idx), " shape: ", x.shape)
 
-        x = rearrange(x, 'n c d h w -> n d h w c')
+        x = rearrange(x, "n c d h w -> n d h w c")
         x = self.norm(x)
-        x = rearrange(x, 'n d h w c -> n c d h w')
+        x = rearrange(x, "n d h w c -> n c d h w")
 
-        if channels == 3:
-            proj = torch.nn.ConvTranspose3d(x.shape[1], channels, (5,4,4), (1,4,4), padding=(4,0,0), dilation=(1,1,1))
-            x = proj(x)
-            # print(x.shape)
-        else:
-            proj = torch.nn.ConvTranspose3d(x.shape[1], channels, (2,4,4), (2,4,4), dilation=(1,1,1))
-            x = proj(x)
-            # print(x.shape)
+        x = self.proj(x)
 
         return x
 
-        
+
 # For testing
 # if __name__ == "__main__":
 #     x = torch.rand(10,8,8,8,768)
@@ -144,7 +167,7 @@ class SwinTransformer3D_up(SwinTransformer3D):
 
 # from models.up_scaling.reverse_st.upsampling import SwinTransformer3D_up
 
-# def main(): 
+# def main():
 
 #     x = torch.rand(10,768,8,8,8)
 #     model = SwinTransformer3D_up()
@@ -152,7 +175,3 @@ class SwinTransformer3D_up(SwinTransformer3D):
 
 # if __name__ == '__main__':
 #     main()
-        
-        
-
-        
