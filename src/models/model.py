021e8162ff2884ca96d18f4f5b506f434ed631f5
Tim Herzig <tim.herzig@hotmail.com>
2023-06-25 11:50:08 +0200

    frozen stages set back to 0

--- src/models/model.py
+++ src/models/model.py
@@ -37,7 +37,7 @@ class Decomposer(pl.LightningModule):
             self.swin = SwinTransformer3D(
                 pretrained=config.model.swin.checkpoint,
                 patch_size=self.model_config.swin.patch_size,
-                frozen_stages=-1,  # =0
+                frozen_stages=0,
             )
             print("Loaded SWIN checkpoint")
             print("-----------------")
